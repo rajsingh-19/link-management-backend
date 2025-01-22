8251371ac69ac92ea321482490b8197a4b73f1b4
Raj Singh <rajsingh.19.vns@gmail.com>
2025-01-22 17:08:59 +0530

    Implemented the db configuration

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const connectDB = require("./config/dbConfig");
 
 const app = express();
 dotenv.config();
+connectDB();
 
 const PORT = process.env.PORT || 5480;
 
@@ -10,6 +13,16 @@ app.get('/', (req, res) =>  {
     res.send("Server established");
 });
 
-app.listen(PORT, () => {
-    console.log("Server is runnning on the port", PORT);
-});    
+//          wait for the db connection before starting the server
+mongoose.connection.once('open', () => {
+    console.log("Database Connected");
+    //      starting the server after the db connection is established
+    app.listen(PORT, () => {
+        console.log("Server is runnning on the port", PORT);
+    });    
+});
+
+//          error handling for db connection issues
+mongoose.connection.on('error', (error) => {
+    console.log(`Database Connection Error : ${error}`);
+});
